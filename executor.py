7b3f7c752700e3a3799a7452a3f004544c16d8c3
ohadkoren <ohadkorenok@gmail.com>
2020-08-24 17:23:07 +0300

    finished clustering, some of parsing. TODO:: clean the code...

--- executor.py
+++ executor.py
@@ -1,9 +1,9 @@
 from metacritic_extraction import CSVParser
 from clustering import cluster_us
-
 hey = "hi"
-# ohad = CSVParser("awards_by_films.csv")
+parser = CSVParser("awards_by_films_shortened.csv")
 # ohad.write_to_file()
 # too = cluster_us(ohad.movies)
-cluster_us()
+
+cluster_us(parser.movies)
 roy = "aaaa"
